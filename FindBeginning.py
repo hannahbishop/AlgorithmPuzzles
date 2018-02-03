9edea7d12690f33b8d87a0c6d0bc976a54dfcc6b
hannahbishop <hannahbananabishop@gmail.com>
2018-02-02 22:19:46 -0800

    Add 2.6

---
+++ FindBeginning.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    def setNext(self, node):
+        self.next = node
+
+def printList(start):
+    curr = start
+    while(curr != None):
+        print(curr.value)
+        curr = curr.next
+    return
+
+def findFirstNode(start):
+    seen = {}
+    curr = start
+    while(curr != None):
+        if(curr.value in seen):
+            return curr
+        else:
+            seen[curr.value] = True
+            curr = curr.next
+
+def main():
+    one = Node(1)
+    two = Node(2)
+    three = Node(3)
+    four = Node(4)
+    five = Node(5)
+    six = Node(6)
+    
+    one.setNext(two)
+    two.setNext(three)
+    three.setNext(four)
+    four.setNext(five)
+    five.setNext(six)
+    six.setNext(five)
+    
+    print(findFirstNode(one).value)
+
+main()
